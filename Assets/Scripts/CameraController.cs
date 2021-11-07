d6ae9d7045390ed1022f9cb292e2c67b6585fab4
Draekdude <43507908+Draekdude@users.noreply.github.com>
2021-11-07 12:22:02 -0600

    12 - Clamping The Camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,14 +1,25 @@
+using System.Runtime.InteropServices;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] BoxCollider2D boxBounds;
     PlayerController player;
+    float halfHeight;
+    float halfWidth;
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType<PlayerController>();
+        SetCameraParameters();
+    }
+
+    private void SetCameraParameters()
+    {
+        halfHeight = Camera.main.orthographicSize;
+        halfWidth = halfHeight * Camera.main.aspect;
     }
 
     // Update is called once per frame
@@ -19,8 +30,22 @@ public class CameraController : MonoBehaviour
 
     private void SetCameraPositionToPlayer()
     {
-        if (player != null) {
-            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        if (player != null)
+        {
+            transform.position = new Vector3(
+                GetXPositionClampedToBoxBoundsMinMax(),
+                GetYPositionClampedToBoxBoundsMinMax(),
+                transform.position.z);
         }
+    }
+
+    private float GetYPositionClampedToBoxBoundsMinMax()
+    {
+        return Mathf.Clamp(player.transform.position.y, boxBounds.bounds.min.y + halfHeight, boxBounds.bounds.max.y - halfHeight);
+    }
+
+    private float GetXPositionClampedToBoxBoundsMinMax()
+    {
+        return Mathf.Clamp(player.transform.position.x, boxBounds.bounds.min.x + halfWidth, boxBounds.bounds.max.x - halfWidth);
     }
 }
