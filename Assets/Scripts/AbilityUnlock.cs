2d4498e352a8313ae4d3d2d34dc73affc5f39caf
Draekdude <43507908+Draekdude@users.noreply.github.com>
2021-11-15 06:02:17 -0600

    29 - Pickup Effect & Text

--- Assets/Scripts/AbilityUnlock.cs
+++ Assets/Scripts/AbilityUnlock.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class AbilityUnlock : MonoBehaviour
 {
@@ -8,6 +9,10 @@ public class AbilityUnlock : MonoBehaviour
     [SerializeField] bool unlockDash;
     [SerializeField] bool unlockBecomeBall;
     [SerializeField] bool unlockDropBomb;
+    [SerializeField] GameObject pickupEffect;
+    [SerializeField] string unlockMessage;
+    [SerializeField] TMP_Text unlockText;
+    
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -15,29 +20,42 @@ public class AbilityUnlock : MonoBehaviour
         {
             PlayerAbilityTracker player = other.GetComponentInParent<PlayerAbilityTracker>();
 
-            if(unlockDoubleJump)
+            if (unlockDoubleJump)
             {
                 player.SetCanDoubleJump(true);
             }
 
-            if(unlockDash)
+            if (unlockDash)
             {
                 player.SetCanDash(true);
             }
 
-            if(unlockBecomeBall)
+            if (unlockBecomeBall)
             {
                 player.SetCanBecomeBall(true);
             }
 
-            if(unlockDropBomb)
+            if (unlockDropBomb)
             {
                 player.SetCanDropBomb(true);
             }
 
+            Instantiate(pickupEffect, transform.position, transform.rotation);
+            SetUnlockText();
             Destroy(gameObject);
-        } 
+        }
     }
+
+    private void SetUnlockText()
+    {
+        Transform parent = unlockText.transform.parent;
+        parent.SetParent(null);
+        parent.position = transform.position;
+        unlockText.text = unlockMessage;
+        unlockText.gameObject.SetActive(true);
+        Destroy(parent.gameObject, 3f);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
