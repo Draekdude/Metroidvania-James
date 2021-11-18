4570c6eb2803deb3c1ac99eeb6bf9f78ca5f1f15
Draekdude <43507908+Draekdude@users.noreply.github.com>
2021-11-17 18:38:26 -0600

    34 - Damaging The Player

---
+++ Assets/Scripts/DamagePlayer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour
+{
+    [SerializeField] int damageAmount = 1;
+    const string Player_Tag = "Player";
+    PlayerHealthController playerHealthController;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == Player_Tag)
+        {
+            playerHealthController = other.gameObject.GetComponent<PlayerHealthController>();
+            DealDamage();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == Player_Tag)
+        {
+            playerHealthController = other.gameObject.GetComponent<PlayerHealthController>();
+            DealDamage();
+        }
+    }
+
+    void DealDamage()
+    {
+        playerHealthController.DamagePlayer(damageAmount);
+    }
+}
