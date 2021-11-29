c0b8b6aef567108c49e09a163f45426bab585a96
Draekdude <43507908+Draekdude@users.noreply.github.com>
2021-11-28 19:52:59 -0600

    52 - Moving The Camera To Position

---
+++ Assets/Scripts/GhostBoss.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostBoss : MonoBehaviour
+{
+    [SerializeField] Transform camPosition;
+    [SerializeField] float camSpeed;
+    CameraController cameraController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraController = FindObjectOfType<CameraController>();
+        cameraController.enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraController.transform.position = Vector3.MoveTowards(cameraController.transform.position, camPosition.transform.position, camSpeed * Time.deltaTime);
+    }
+}
